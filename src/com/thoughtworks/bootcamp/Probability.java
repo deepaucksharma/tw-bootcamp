900c771064ab3a7f099ba73c74fd81876290e632
amit dash <dbamit@thoughtworks.com>
2016-05-30 17:50:19 +0530

    Probability: more methods (and / or) and tests added

--- src/com/thoughtworks/bootcamp/Probability.java
+++ src/com/thoughtworks/bootcamp/Probability.java
@@ -2,29 +2,35 @@ package com.thoughtworks.bootcamp;
 
 /**
  * Created by amitdash on 5/30/16.
+ * Job: Represents chance of an event, encapsulates the probability operations
  */
 public class Probability {
+    public static final int MAXIMUM_CHANCE = 1;
+    private double chance;
 
-    public double getOdds() {
-        return odds;
-    }
-
-    double odds;
-
-    public Probability(double value) {
-        if (value > 1 || value < 0) {
+    public Probability(double chance) {
+        if (chance > 1 || chance < 0) {
             throw new RuntimeException("Invalid probability");
         }
 
-        this.odds = value;
+        this.chance = chance;
+    }
+
+    private double round(double chance)
+    {
+        return Math.round(chance * 10000) / 10000.0;
     }
 
     public Probability negate() {
-        return new Probability(1D - odds);
+        return new Probability(this.round(MAXIMUM_CHANCE - chance));
     }
 
-    public Probability and(Probability probability) {
-        return new Probability(this.odds * probability.odds);
+    public Probability and(Probability that) {
+        return new Probability(this.round(this.chance * that.chance));
+    }
+
+    public Probability or(Probability that) {
+        return new Probability(this.round(this.chance + that.chance));
     }
 
     @Override
@@ -34,17 +40,12 @@ public class Probability {
 
         Probability that = (Probability) o;
 
-        return Double.compare(that.odds, odds) == 0;
-
+        return Double.compare(that.chance, chance) == 0;
     }
 
     @Override
     public int hashCode() {
-        long temp = Double.doubleToLongBits(odds);
+        long temp = Double.doubleToLongBits(chance);
         return (int) (temp ^ (temp >>> 32));
     }
-
-    //    public Probability or(Probability p1, Probability p2) {
-//        return 0;
-//    }
 }
--- tests/com/thoughtworks/bootcamp/ProbabilityTest.java
+++ tests/com/thoughtworks/bootcamp/ProbabilityTest.java
@@ -44,15 +44,18 @@ public class ProbabilityTest {
     }
 
     @Test
+    public void testDoubleNegateOperation() {
+        Assert.assertThat(new Probability(0.2).negate().negate(), is(new Probability(0.2)));
+    }
+
+    @Test
     public void testAndOperation() {
         Assert.assertThat(new Probability(0.3).and(new Probability(0.3)), is(new Probability(0.09)));
     }
 
-//    @Test
-//    public void orTest()
-//    {
-//        double p1 = 0.6,p2=.1;
-//        double expected = p1 + p2;
-//        Assert.assertThat(probEng.or(p1, p2), is(expected));
-//    }
+    @Test
+    public void orTest()
+    {
+        Assert.assertThat(new Probability(0.3).or(new Probability(0.3)), is(new Probability(0.6)));
+    }
 }
